c99272bb68db3c2e636b25c9ddf3ef47c01d0abf
RAMAZAN DOGAN <doganrmzn40@gmail.com>
2023-12-27 17:18:52 +0300

    Register and Login endpoints, JWT, Bcrypt

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,38 +1,62 @@
+import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import axios from 'axios'
 //types
 import type { userLogin } from '../types'
+//toast
+import { toast } from 'react-hot-toast'
 
 function Login() {
   //states
-  const [user, setUser] = useState<userLogin>({
+  const [data, setData] = useState<userLogin>({
     email: '',
     password: ''
   })
 
+  //nav
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const navigate = useNavigate()
   //fonk
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    axios.get('/')
+    const { email, password } = data
+    try {
+      const { data } = await axios.post('/login', {
+        email,
+        password
+      })
+      if (data.error) {
+        toast.error(data.error)
+      } else {
+        setData({
+          email: '',
+          password: ''
+        })
+        toast.success("You're logged in successfully")
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <div className="h-100vh w-100vw flex flex-col items-center justify-center">
-      <h1 className="text-31px">Register</h1>
+      <h1 className="text-31px">Login</h1>
       <form className="flex flex-col" onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
         <input
           type="email"
           id="email"
-          value={user.email}
-          onChange={e => setUser({ ...user, email: e.target.value })}
+          value={data.email}
+          onChange={e => setData({ ...data, email: e.target.value })}
         />
         <label htmlFor="password">Password</label>
         <input
           type="password"
           id="password"
-          value={user.password}
-          onChange={e => setUser({ ...user, password: e.target.value })}
+          value={data.password}
+          onChange={e => setData({ ...data, password: e.target.value })}
         />
         <button type="submit">Login</button>
       </form>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,52 +1,72 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 //types
 import type { userRegister } from '../types'
+//toast
+import { toast } from 'react-hot-toast'
 
 function Register() {
   //states
-  const [user, setUser] = useState<userRegister>({
+  const [data, setData] = useState<userRegister>({
     name: '',
-    userName: '',
     email: '',
     password: ''
   })
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const navigate = useNavigate()
+
   //fonk
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    const { name, email, password } = data
+    try {
+      const { data } = await axios.post('/register', {
+        name,
+        email,
+        password
+      })
+      if (data.error) {
+        toast.error(data.error)
+      } else {
+        setData({
+          email: '',
+          name: '',
+          password: ''
+        })
+        toast.success("You're registered successfully")
+        navigate('/login')
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <div className="h-100vh w-100vw flex flex-col items-center justify-center">
       <h1>Register</h1>
       <form className="flex flex-col" onSubmit={handleSubmit}>
-        <label htmlFor="username">Name</label>
+        <label htmlFor="name">Name</label>
         <input
           type="text"
           id="name"
-          value={user.name}
-          onChange={e => setUser({ ...user, name: e.target.value })}
-        />
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          value={user.userName}
-          onChange={e => setUser({ ...user, userName: e.target.value })}
+          value={data.name}
+          onChange={e => setData({ ...data, name: e.target.value })}
         />
         <label htmlFor="email">Email</label>
         <input
           type="email"
           id="email"
-          value={user.email}
-          onChange={e => setUser({ ...user, email: e.target.value })}
+          value={data.email}
+          onChange={e => setData({ ...data, email: e.target.value })}
         />
         <label htmlFor="password">Password</label>
         <input
           type="password"
           id="password"
-          value={user.password}
-          onChange={e => setUser({ ...user, password: e.target.value })}
+          value={data.password}
+          onChange={e => setData({ ...data, password: e.target.value })}
         />
         <button type="submit">Register</button>
       </form>
